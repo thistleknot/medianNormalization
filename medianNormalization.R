6babec227e79fcd9a9ae16ccf86ce2b93434b232
thistleknot <rstudio@rstudio.localhost.localdomain>
2020-08-25 01:29:39 +0000

    added 2 new methods (blue, orange) green was old method (non linear)

--- medianNormalization.R
+++ medianNormalization.R
@@ -1,11 +1,13 @@
 library(parallel)
 require(ggplot2)
 require(gridExtra)
+library(crayon)
 
+{
 numDice = sample(3:10,1)
 dice = c(4,6,8,10,12,20,100)
-#numPositions = sample(c(50),1)
-numPositions = 50
+numPositions = sample(c(50),1)
+#numPositions = 50
 
 diceSet = sample(dice,numDice,replace=TRUE)
 
@@ -43,7 +45,7 @@ factorVal <- expectedQuartile/average
 
 #Confirmed
 
-#quantile(rnorm(100000),probs=c(.25))
+#quantile(rnorm(10000000),probs=c(.25))
 
 lowerFactor <- lowerAvg/average*factorVal
 upperFactor <- upperAvg/average*factorVal
@@ -60,28 +62,79 @@ reScaled[upper] <- zScores[upper]*lowerFactor
 mean(zScores)
 mean(reScaled)
 
-df <- data.frame(rolledSet,pnorm(zScores),pnorm(reScaled))
+twentyFivePercent <- m-lowerAvg*stratifiedMAD
+seventyFivePercent <- m+upperAvg*stratifiedMAD
+
+cat(blue(paste ("\n","blue 25/75%")))
+cat(blue(paste ("\n",twentyFivePercent,seventyFivePercent)))
+
+twentyFivePercentRescaled <- m-expectedQuartile*stratifiedMAD*factorVal
+seventyFivePercentRescaled <- m+expectedQuartile*stratifiedMAD*factorVal
+
+m
+#linear, but 50% is no longe median, it's close and within 25 to 75%
+newMean <- mean(c(twentyFivePercent,seventyFivePercent))
+newSdev <- (seventyFivePercent-twentyFivePercent)/(expectedQuartile*2)
+
+newSetZ <- (rolledSet-newMean)/newSdev
+plot(newSetZ,rolledSet)
+newSetP <- pnorm(newSetZ)
+plot(newSetP,rolledSet)
+
+mean(c(twentyFivePercentRescaled,seventyFivePercentRescaled))
+
+#linear, preserves median at 50%
+newSdevRescaled <-  (seventyFivePercentRescaled-twentyFivePercentRescaled)/(expectedQuartile*2)
+newSetZRescaled <- (rolledSet-m)/newSdevRescaled
+plot(newSetZRescaled,rolledSet)
+newSetPRescaled <- pnorm(newSetZRescaled)
+plot(newSetPRescaled,rolledSet)
+
+cat(yellow(paste ("\n","orange 25/75%")))
+cat(yellow(paste ("\n",twentyFivePercentRescaled,seventyFivePercentRescaled)))
+
+cat(paste("\n","actual","\n"))
+print(summary(rolledSet))
+
+df <- data.frame(rolledSet,pnorm(zScores),pnorm(reScaled),newSetP,newSetPRescaled)
 
 plot1 <- ggplot(df, aes(rolledSet)) +                  # basic graphical object
   geom_point(aes(y=pnorm.zScores.), colour="red") +  # first layer
-  geom_line(aes(y=pnorm.reScaled.), colour="green")  # second layer
+  geom_line(aes(y=pnorm.reScaled.), colour="green") +  # second layer
+  geom_line(aes(y=newSetP), colour="blue") + # third layer
+  geom_line(aes(y=newSetPRescaled), colour="orange")  # fourth layer
 
-df <- data.frame(rolledSet,zScores,reScaled)
+df <- data.frame(rolledSet,zScores,reScaled,newSetZRescaled,newSetZ)
 
 plot2 <- ggplot(df, aes(rolledSet)) +                  # basic graphical object
   geom_point(aes(y=zScores), colour="red") +  # first layer
-  geom_line(aes(y=reScaled), colour="green")  # second layer
+  geom_line(aes(y=reScaled), colour="green") + # second layer
+  geom_line(aes(y=newSetZRescaled), colour="blue") + # third layer
+  geom_line(aes(y=newSetZ), colour="orange")  # fourth layer
 
-df <- data.frame(1:numPositions,zScores,reScaled)
+df <- data.frame(1:numPositions,zScores,reScaled,newSetZRescaled,newSetZ)
 
 plot3 <- ggplot(df, aes(X1.numPositions)) +                    # basic graphical object
   geom_point(aes(y=zScores), colour="red") +  # first layer
-  geom_line(aes(y=reScaled), colour="green")  # second layer
+  geom_line(aes(y=reScaled), colour="green") +  # second layer
+  geom_line(aes(y=newSetZRescaled), colour="blue") + # third layer
+  geom_line(aes(y=newSetZ), colour="orange")  # fourth layer
 
 grid.arrange(plot1, plot2, plot3, ncol=2, nrow=2)
 
-summary(pnorm(zScores))
-summary(pnorm(reScaled))
+cat (red(paste ("\n","Red:", "pnorm(ZScores), median 50%, avg !50% linear:","\n", collapse="")))
+print(summary(pnorm(zScores)))
+print(summary(zScores))
 
-summary(zScores)
-summary(reScaled)
+cat (green(paste ("\n","Green:", "pnorm(reScaled), median 50%, avg 50% nonlinear:","\n", collapse="")))
+print(summary(pnorm(reScaled)))
+print(summary(reScaled))
+
+cat (blue(paste ("\n","Blue:", "newSetP, median ~50%, avg 50%, linear:","\n", collapse="")))
+print(summary(newSetP))
+print(summary(newSetZ))
+
+cat (yellow(paste ( "\n","Orange:","newSetPRescaled, median 50%, avg !50%, linear:", "\n", collapse="")))
+print(summary(newSetPRescaled))
+print(summary(newSetZRescaled))
+}
